a38695ef92ccde31539dd0c85d91c0dbdf9b30fa
Stefan Insam <ramsesoriginal@gmail.com>
2014-10-26 02:33:47 +0200

    screwed up initial commit. here it is.

---
+++ Game/Assets/Scripts/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+
+	public void StartGameplay () {
+		Application.LoadLevel("game");
+	}
+
+	public void StartMenu () {
+		Application.LoadLevel("startup scene");
+	}
+}
